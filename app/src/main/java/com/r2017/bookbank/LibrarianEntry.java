f44e8ab2fc701c43c958680bb8b20b49e0d954c9
Harishwarrior <warriorharish95668@gmail.com>
2020-02-29 22:34:41 +0530

    fixed librarian entry file,entry of data works now

--- app/src/main/java/com/r2017/bookbank/LibrarianEntry.java
+++ app/src/main/java/com/r2017/bookbank/LibrarianEntry.java
@@ -2,7 +2,6 @@ package com.r2017.bookbank;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -15,19 +14,19 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LibrarianEntry extends AppCompatActivity {
 
     SQLiteDatabase sqLiteDatabaseObj;
-    EditText editTextName, editTextPhoneNumber;
-    String NameHolder, NumberHolder, SQLiteDataBaseQueryHolder;
+    EditText editTextBookName, editTextAuthorName;
+    String BookHolder, AuthorHolder, SQLiteDataBaseQueryHolder;
     Button EnterData;
     Boolean EditTextEmptyHold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_librarian_entry);
 
-        EnterData = findViewById(R.id.btnAddBook;
-        editTextName = findViewById(R.id.);
-        editTextPhoneNumber = findViewById(R.id.editText2);
+        EnterData = findViewById(R.id.btnAddBook);
+        editTextBookName = findViewById(R.id.etBookName);
+        editTextAuthorName = findViewById(R.id.etAuthorName);
 
         EnterData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,43 +58,40 @@ public class LibrarianEntry extends AppCompatActivity {
 
     public void SQLiteTableBuild(){
 
-        sqLiteDatabaseObj.execSQL("CREATE TABLE IF NOT EXISTS AndroidJSonTable(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR, phone_number VARCHAR);");
+        sqLiteDatabaseObj.execSQL("CREATE TABLE IF NOT EXISTS AndroidJSonTable(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Book_name VARCHAR, Author_name VARCHAR);");
 
     }
 
     public void CheckEditTextStatus(){
 
-        NameHolder = editTextName.getText().toString() ;
-        NumberHolder = editTextPhoneNumber.getText().toString();
+        BookHolder = editTextBookName.getText().toString() ;
+        AuthorHolder = editTextAuthorName.getText().toString();
 
-        EditTextEmptyHold = !TextUtils.isEmpty(NameHolder) && !TextUtils.isEmpty(NumberHolder);
+        EditTextEmptyHold = !TextUtils.isEmpty(BookHolder) && !TextUtils.isEmpty(AuthorHolder);
     }
 
     public void InsertDataIntoSQLiteDatabase(){
 
-        if(EditTextEmptyHold == true)
+        if(EditTextEmptyHold)
         {
 
-            SQLiteDataBaseQueryHolder = "INSERT INTO AndroidJSonTable (name,phone_number) VALUES('"+NameHolder+"', '"+NumberHolder+"');";
+            SQLiteDataBaseQueryHolder = "INSERT INTO AndroidJSonTable (Book_name,Author_name) VALUES('"+BookHolder+"', '"+AuthorHolder+"');";
 
             sqLiteDatabaseObj.execSQL(SQLiteDataBaseQueryHolder);
 
-            Toast.makeText(MainActivity.this,"Data Inserted Successfully", Toast.LENGTH_LONG).show();
+            Toast.makeText(LibrarianEntry.this,"Data Inserted Successfully", Toast.LENGTH_LONG).show();
 
         }
-        else {
-
-            Toast.makeText(MainActivity.this,"Please Fill All The Required Fields.", Toast.LENGTH_LONG).show();
-
-        }
+        else
+            Toast.makeText(LibrarianEntry.this, "Please Fill All The Required Fields.", Toast.LENGTH_LONG).show();
 
     }
 
     public void EmptyEditTextAfterDataInsert(){
 
-        editTextName.getText().clear();
+        editTextBookName.getText().clear();
 
-        editTextPhoneNumber.getText().clear();
+        editTextAuthorName.getText().clear();
 
     }
 
